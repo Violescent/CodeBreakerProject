11d451a6f4668466144769137e0242ea5a1ece77
Violescent <period1@mw-b203-s27.mwinst.visalia.k12.ca.us>
2017-05-23 09:19:11 -0700

    Next Steps

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,10 +3,17 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
+    if answer.value == '' || attempt.value == '' {
+      setHiddenFields();
+    }
     //add functionality to guess function here
 }
 
 function setHiddenFields () {
-  answer.value = Math.floor(Math.random() * 10000);
+  answer.value = Math.floor(Math.random() * 10000).toString();
+  while(answer.value.length < 4) {
+    answer.value = "0" + answer.value;
+  }
+  attempt.value = "0";
 }
 //implement new functions here
