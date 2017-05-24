6c1f917bbe9e77907d7286a733d22c6a8d3f967a
Violescent <period1@mw-b203-s27.mwinst.visalia.k12.ca.us>
2017-05-24 09:11:05 -0700

    New

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,7 +6,31 @@ function guess() {
     if answer.value == '' || attempt.value == '' {
       setHiddenFields();
     }
-    //add functionality to guess function here
+
+if(!validateInput(input.value)) {
+  return;
+  }
+  attempt.value++;
+}
+
+function getResults(input) {
+  let html = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+  for(i = 0; i < imput.length; i++) {
+    if(input.charAt(i) ==  answer.value.charAt(i)) {
+      html += '<span class="glyphicon glyphicon-ok"></span>';
+    } else if(answer.value.indexOf(input.charAt(i)) . -1) {
+      html += '<span class="glyphicon glyphicon-transfer"></span>';
+    } else {
+      html += '<span class="glyphicon glyphicon-remove"></span>';
+    }
+  }
+  html += '</div></div>';
+  document.getElementById('results').innerHTML += html;
+
+  if(input == answer.value) {
+    return true;
+  }
+    return false; 
 }
 
 function setHiddenFields () {
@@ -20,4 +44,11 @@ function setHiddenFields () {
 function setMessage (message) {
   document.getElementById('message').innerHTML = message;
 }
-//implement new functions here
+
+function validateInput(input) {
+  if(input.length != 4) {
+    setMessage('Guess must be exactly 4 characters long.');
+    return false;
+  }
+  return true;
+}
